154346ddc695eefa2eb426d42c6d6b485e7e8255
pavanprajapati62 <pavanprajapati62@gmail.com>
2025-02-04 15:33:16 +0530

    Fix: db port

--- config/db.js
+++ config/db.js
@@ -6,8 +6,15 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
